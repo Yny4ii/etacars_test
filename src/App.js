20331ad484a1a3fbd14026c3bb6dbad4b117f685
ilya <ilya.semyonov.03@mail.ru>
2022-08-14 20:42:33 +0300

    feat: add redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import './App.css';
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import {getCurrencies} from "./redux/slices/currencySlices";
+import {CurrencyTable} from "./components/currencyTable/currencyTable";
+import {Header} from "./components/header/header";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCurrencies())
+    }, [dispatch])
+    return (
+        <div className="App">
+            <Header/>
+            <CurrencyTable/>
+        </div>
+    );
 }
 
 export default App;
