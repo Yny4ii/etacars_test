7491e9f4415a655707a068f6f6c2540dddf0bfc5
ilya <ilya.semyonov.03@mail.ru>
2022-08-15 21:38:56 +0300

    feat: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import './App.css';
-import {useEffect} from "react";
-import {getCurrencies} from "./redux/slices/currencySlices";
+import React, {useEffect} from 'react';
+import {getCurrencies, getCurrencyHistory} from "./redux/slices/currencySlices";
 import {CurrencyTable} from "./components/currencyTable/currencyTable";
 import {Header} from "./components/header/header";
 import {useAppDispatch} from "./hooks/hooks";
+import {LineChart} from "./components/lineChart/lineChart";
 
 export const App = (): JSX.Element => {
+
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(getCurrencies())
+        dispatch(getCurrencyHistory('bitcoin'))
+
     }, [dispatch])
+
     return (
         <div className="App">
             <Header/>
             <CurrencyTable/>
+            <LineChart/>
         </div>
     );
 }
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -25,7 +25,7 @@ const Modal = ({setActive}: ModalProps) => {
     return (
         <div className='modal' onClick={() => setActive(false)}>
             <div className="modal__content" onClick={e => e.stopPropagation()}>
-                <p>Enter the value and click 'add'</p>
+                <p>Enter the value and click add</p>
                 <div className='modal__input-field'>
                     <input onChange={e => onChangeInput(e)} value={input} autoFocus={true} className='modal__input'
                            placeholder='Enter the value...'/>
