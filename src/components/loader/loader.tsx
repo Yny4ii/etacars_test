2f90ad5358a69bf1d71d5ae87f3cf05f795d1ddb
ilya <ilya.semyonov.03@mail.ru>
2022-08-16 20:26:48 +0300

    fix: loader styles

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Loader = () => {
     return (
-        <div className='container'>
+        <div className='box'>
             <span className='loader'></span>
         </div>
     );
