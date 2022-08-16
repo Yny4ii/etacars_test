9563ef31e683ad431dbba7ebb07ccc5dfa9727ee
ilya <ilya.semyonov.03@mail.ru>
2022-08-16 20:27:25 +0300

    fix: header html & css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,6 @@ export const Header = () => {
     const currencies = useAppSelector(state => state.currencyReducer.currencies).slice(0, 3);
     return (
         <header className='header'>
-            <div className='header__container'>
                 <ul className='top-currency'>
                     {currencies.map(e => (
                         <li className="top-currency__item" key={e.id}>{e.name} - ${floatFormat(e.priceUsd)}</li>
@@ -15,7 +14,6 @@ export const Header = () => {
                 <div className="header__wallet">
                     123
                 </div>
-            </div>
 
         </header>
     );
