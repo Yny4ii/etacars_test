9ad4097e924c5f65ef6061df9e3d755da1f01018
ilya <ilya.semyonov.03@mail.ru>
2022-08-17 16:38:13 +0300

    fix: change header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { floatFormat } from "../../helpers/floatFormat";
 import { useAppSelector } from "../../hooks/hooks";
+import { calcInitialWallet } from "../../helpers/calcWallet";
+import { WalletModal } from "../walletModal/walletModal";
 
 export const Header = () => {
+  const [modalActive, setModalActive] = useState<boolean>(false);
+
+  const { currencies: walletCurrency } = useAppSelector(
+    (state) => state.walletReducer
+  );
   const currencies = useAppSelector(
     (state) => state.currencyReducer.currencies
   ).slice(0, 3);
@@ -15,8 +22,19 @@ export const Header = () => {
           </li>
         ))}
       </ul>
-      <div className="header__wallet">123</div>
+      <div className="header__wallet" onClick={() => setModalActive(true)}>
+        <div className='header__wallet-info'>
+          <div>${floatFormat(calcInitialWallet(walletCurrency))}</div>
+          <div>12%</div>
+          <div>+$163</div>
+        </div>
+      </div>
+      {modalActive && (
+        <WalletModal
+          walletCurrency={walletCurrency}
+          setActive={setModalActive}
+        />
+      )}
     </header>
   );
 };
-
