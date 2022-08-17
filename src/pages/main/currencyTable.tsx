e1aa88042ce9d32e494b4adac236e246710c61b9
ilya <ilya.semyonov.03@mail.ru>
2022-08-17 22:22:35 +0300

    feat: responsive styles

--- src/pages/main/currencyTable.tsx
+++ src/pages/main/currencyTable.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { CurrencyItem } from "../../components/currencyItem/currencyItem";
 import Modal from "../../components/modal/modal";
-import { useAppSelector } from "../../hooks/hooks";
+import { useAppSelector, useWindowWidth } from "../../hooks/hooks";
 import { Loader } from "../../components/loader/loader";
 import { Pagination } from "../../components/pagination/Pagination";
 import { Currency } from "../../interfaces/Currency";
+import { floatFormat } from "../../helpers/floatFormat";
 
 export const CurrencyTable = () => {
   const { currencies, status } = useAppSelector(
     (state) => state.currencyReducer
   );
+
+  const width = useWindowWidth();
 
   const [modalActive, setModalActive] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -39,11 +42,23 @@ export const CurrencyTable = () => {
                 <th className="table__item">#</th>
                 <th className="table__item">Coin</th>
                 <th className="table__item">Price</th>
-                <th className="table__item">Market cap</th>
-                <th className="table__item">VWAP (24Hr)</th>
-                <th className="table__item">Supply</th>
-                <th className="table__item">Volume (24Hr)</th>
-                <th className="table__item">Change (24Hr)</th>
+                {width > 780 ? (
+                  <th className="table__item">Market cap</th>
+                ) : null}
+
+                {width > 660 ? (
+                  <th className="table__item">VWAP (24Hr)</th>
+                ) : null}
+                {width > 560 ? <th className="table__item">Supply</th> : null}
+
+                {width > 890 ? (
+                  <th className="table__item">Volume (24Hr)</th>
+                ) : (
+                  ""
+                )}
+                {width > 400 ? (
+                  <th className="table__item">Change (24Hr)</th>
+                ) : null}
               </tr>
             </thead>
             <tbody>
@@ -70,4 +85,3 @@ export const CurrencyTable = () => {
     </>
   );
 };
-
