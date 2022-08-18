1bc3ae4712412b4bd87e8a35cc345c6e77c9f2ca
ilya <ilya.semyonov.03@mail.ru>
2022-08-18 11:29:36 +0300

    fix: pagination crush bug

--- src/helpers/floatFormat.ts
+++ src/helpers/floatFormat.ts
@@ -1,5 +1,8 @@
 export const floatFormat = (value: string | number): string => {
   const number = typeof value === "string" ? parseFloat(value) : value;
+  if (number === null) {
+    return "0";
+  }
   if (number > 1000000000) {
     return `${(number / 1000000000).toFixed(2)}B`;
   }
